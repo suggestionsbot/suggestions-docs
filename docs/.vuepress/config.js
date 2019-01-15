06933716a707d44ec361aca08d65b043b9ccaafa
acollierr17 <gamemaniac101@gmail.com>
2019-01-15 00:04:00 -0500

    some more changes that'll probably not work

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,15 @@
 module.exports = {
     title: 'Suggestions Documentation',
     description: 'The official documentation for the Suggestions bot. The only suggestions bot you\'ll ever need. Simple usage and management of suggestions for public and staff use.',
+    dest: "public",
+    base: "/docs/",
     themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'Discord', link: 'https://discord.gg/g7wr8xb' },
-            { text: 'Patreon', link: 'https://patreon.com/acollierr17' }
-        ],
+        // nav: [
+        //     { text: 'Home', link: '/' },
+        //     { text: 'Guide', link: '/guide/' },
+        //     { text: 'Discord', link: 'https://discord.gg/g7wr8xb' },
+        //     { text: 'Patreon', link: 'https://patreon.com/acollierr17' }
+        // ],
         sidebar: {
             '/guide/': [
                 '',
